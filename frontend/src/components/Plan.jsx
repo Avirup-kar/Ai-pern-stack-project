73cf2359837bfa1e561367f1b11ce74b3acfa9d9
Avirup kar <avirupk57@gmail.com>
2026-02-15 21:18:32 +0530

    Update Plan.jsx

--- frontend/src/components/Plan.jsx
+++ frontend/src/components/Plan.jsx
@@ -6,7 +6,7 @@ const Plan = () => {
     <div className='max-w-2x1 mx-auto z-20 mt-10 mb-30 px-15'>
       <div className='text-center'>
         <h2 className='text-slate-700 text-[42px] font-semibold'> Choose your plan</h2>
-        <p className='text-gray-500 max-w-lg mx-auto'>Start for free and scale up as you grow. Find the perfect plan for your content creation needs.</p>
+        <p className='text-gray-500 max-w-lg mx-auto'>Start for <b>free</b> and scale up as you grow. Find the perfect <b>plan</b> for your content creation needs.</p>
       </div>
 
       <div className='mt-14 mx-auto  max-w-170'>
